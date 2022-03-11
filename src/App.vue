73937b181022e9a94b31e8b9510178ea5c3a5d1a
wanglin2 <1013335014@qq.com>
2022-03-11 13:36:26 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,222 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="container">
+    <div class="drawBox" ref="drawBox"></div>
+  </div>
 </template>
+
+<script setup>
+import Konva from "konva";
+import { onMounted, ref } from "vue";
+
+// ------------------数据---------------------
+
+// 二叉树
+const binaryTree = {
+  name: "0",
+  left_child: {
+    name: "0-1",
+  },
+  right_child: {
+    name: "0-2",
+    left_child: {
+      name: "0-2-1",
+      left_child: {
+        name: "0-2-1-1",
+        left_child: {
+          name: "0-2-1-1-1",
+        },
+      },
+    },
+  },
+};
+// 多叉树
+const treeData = {
+  name: "0",
+  children: [
+    {
+      name: "0-1",
+      children: [
+        {
+          name: "0-1-1",
+          children: [
+            {
+              name: "0-1-1-1",
+              children: [
+                {
+                  name: "0-1-1-1-1",
+                  children: [
+                    {
+                      name: "0-1-1-1-1",
+                      children: [
+                        
+                      ],
+                    },
+                    {
+                      name: "0-1-1-1-1-2",
+                      children: [],
+                    }
+                  ],
+                },
+                {
+                  name: "0-1-1-1-2",
+                  children: [],
+                }
+              ],
+            },
+            {
+              name: "0-1-1-2",
+              children: [],
+            }
+          ],
+        },
+        {
+          name: "0-1-2",
+          children: [],
+        }
+      ],
+    },
+    {
+      name: "0-2",
+      children: [
+        {
+          name: "0-2-1",
+          children: [
+            {
+              name: "0-2-1-1",
+              children: [
+                
+              ],
+            },
+            {
+              name: "0-2-1-2",
+              children: [],
+            }
+          ],
+        },
+        {
+          name: "0-2-2",
+          children: [],
+        }
+      ],
+    }
+  ],
+};
+
+// ---------------工具方法-----------------
+
+const drawBox = ref(null);
+const NODE_SPACE = 50;
+let layer = null;
+// 初始化舞台
+const init = () => {
+  let stage = new Konva.Stage({
+    container: drawBox.value,
+    width: 800,
+    height: 800,
+  });
+  layer = new Konva.Layer();
+  stage.add(layer);
+};
+// 绘制节点
+const drawNode = (x, y) => {
+  let circle = new Konva.Circle({
+    x,
+    y,
+    radius: 10,
+    fill: "#fff",
+    stroke: "black",
+    strokeWidth: 2,
+  });
+  layer.add(circle);
+  layer.draw();
+};
+// 绘制连接线
+const drawLine = (a, b) => {
+  let line = new Konva.Line({
+    points: [a.x, a.y, b.x, b.y],
+    stroke: "black",
+    strokeWidth: 2,
+    lineCap: "round",
+    lineJoin: "round",
+  });
+  layer.add(line);
+  layer.draw();
+};
+// 深拷贝
+const deepCopy = (data) => {
+  return JSON.parse(JSON.stringify(data));
+};
+// 绘制
+const draw = (tree) => {};
+// 绘制二叉树
+const renderBinaryTree = (tree) => {
+  if (tree.left_child) {
+    drawLine(tree, tree.left_child);
+    renderBinaryTree(tree.left_child);
+  }
+  if (tree.right_child) {
+    drawLine(tree, tree.right_child);
+    renderBinaryTree(tree.right_child);
+  }
+  drawNode(tree.x, tree.y);
+};
+// 绘制多叉树
+const renderTree = (tree) => {
+  if (tree.children && tree.children.length > 0) {
+    tree.children.forEach((child) => {
+      drawLine(tree, child);
+      renderTree(child)
+    })
+  }
+  drawNode(tree.x, tree.y);
+}
+
+// ------------算法实现-----------------
+
+// 算法1
+const useAlgorithm1 = () => {
+  const tree = deepCopy(binaryTree);
+  let i = NODE_SPACE;
+  const knuth_layout = (tree, depth) => {
+    if (tree.left_child) {
+      knuth_layout(tree.left_child, depth + 1);
+    }
+    tree.x = i;
+    tree.y = depth * NODE_SPACE + NODE_SPACE;
+    i += NODE_SPACE;
+    if (tree.right_child) {
+      knuth_layout(tree.right_child, depth + 1);
+    }
+  };
+  knuth_layout(tree, 0);
+  renderBinaryTree(tree);
+};
+
+// 算法2
+const useAlgorithm2 = () => {
+  const tree = deepCopy(treeData);
+  const nexts = []
+  const minimum_ws = (tree, depth = 0) => {
+      if (nexts[depth] === undefined) {
+          nexts[depth] = NODE_SPACE
+      }
+      tree.x = nexts[depth]
+      tree.y = depth * NODE_SPACE + NODE_SPACE
+      nexts[depth] += NODE_SPACE
+      tree.children.forEach((child) => {
+          minimum_ws(child, depth + 1)
+      })
+  }
+  minimum_ws(tree)
+  renderTree(tree)
+}
+
+onMounted(() => {
+  init();
+  // useAlgorithm1();
+  useAlgorithm2()
+});
+</script>
 
 <style>
 #app {
@@ -17,5 +226,17 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.drawBox {
+  border: 1px solid #000;
+  width: 800px;
+  height: 800px;
 }
 </style>
