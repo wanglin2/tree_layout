734513a4baf184a51c2694d8d230dcd0542d3eac
wanglin2 <1013335014@qq.com>
2022-03-11 16:25:26 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -47,32 +47,30 @@ const treeData = {
                   children: [
                     {
                       name: "0-1-1-1-1",
-                      children: [
-                        
-                      ],
+                      children: [],
                     },
                     {
                       name: "0-1-1-1-1-2",
                       children: [],
-                    }
+                    },
                   ],
                 },
                 {
                   name: "0-1-1-1-2",
                   children: [],
-                }
+                },
               ],
             },
             {
               name: "0-1-1-2",
               children: [],
-            }
+            },
           ],
         },
         {
           name: "0-1-2",
           children: [],
-        }
+        },
       ],
     },
     {
@@ -83,22 +81,20 @@ const treeData = {
           children: [
             {
               name: "0-2-1-1",
-              children: [
-                
-              ],
+              children: [],
             },
             {
               name: "0-2-1-2",
               children: [],
-            }
+            },
           ],
         },
         {
           name: "0-2-2",
           children: [],
-        }
+        },
       ],
-    }
+    },
   ],
 };
 
@@ -165,10 +161,20 @@ const renderTree = (tree) => {
   if (tree.children && tree.children.length > 0) {
     tree.children.forEach((child) => {
       drawLine(tree, child);
-      renderTree(child)
-    })
+      renderTree(child);
+    });
   }
   drawNode(tree.x, tree.y);
+};
+const handleTree = (tree) => {
+  tree.x *= NODE_SPACE
+  tree.y *= NODE_SPACE
+  tree.y += NODE_SPACE
+  if (tree.children && tree.children.length > 0) {
+    tree.children.forEach((child) => {
+      handleTree(child)
+    });
+  }
 }
 
 // ------------算法实现-----------------
@@ -195,26 +201,90 @@ const useAlgorithm1 = () => {
 // 算法2
 const useAlgorithm2 = () => {
   const tree = deepCopy(treeData);
-  const nexts = []
+  const nexts = [];
   const minimum_ws = (tree, depth = 0) => {
-      if (nexts[depth] === undefined) {
-          nexts[depth] = NODE_SPACE
-      }
-      tree.x = nexts[depth]
-      tree.y = depth * NODE_SPACE + NODE_SPACE
-      nexts[depth] += NODE_SPACE
-      tree.children.forEach((child) => {
-          minimum_ws(child, depth + 1)
-      })
+    if (nexts[depth] === undefined) {
+      nexts[depth] = NODE_SPACE;
+    }
+    tree.x = nexts[depth];
+    tree.y = depth * NODE_SPACE + NODE_SPACE;
+    nexts[depth] += NODE_SPACE;
+    tree.children.forEach((child) => {
+      minimum_ws(child, depth + 1);
+    });
+  };
+  minimum_ws(tree);
+  renderTree(tree);
+};
+
+// 算法3
+const useAlgorithm3 = () => {
+  class DrawTree {
+    constructor(tree, depth = 0) {
+      this.x = -1;
+      this.y = depth;
+      this.tree = tree;
+      this.children = (tree.children || []).map((child) => {
+        return new DrawTree(child, depth + 1);
+      });
+      this.mod = 0;
+    }
   }
-  minimum_ws(tree)
+  const setup = (tree, depth = 0, nexts = {}, offset = {}) => {
+    if (tree.children && tree.children.length > 0) {
+      tree.children.forEach((child) => {
+        setup(child, depth + 1, nexts, offset);
+      });
+    }
+    // 设置y坐标为节点深度
+    tree.y = depth;
+    let place;
+    // 没有子节点
+    if (!tree.children || tree.children.length <= 0) {
+      place = nexts[depth] || 0;
+      tree.x = place;
+    } else if (tree.children && tree.children.length === 1) {// 只有一个子节点
+      place = tree.children[0].x - 1;
+    } else {// 有多个子节点
+      let s = tree.children[0].x + tree.children[1].x;
+      place = s / 2;
+    }
+    offset[depth] = Math.max(offset[depth] || 0, (nexts[depth] || 0) - place);
+    if (tree.children && tree.children.length > 0) {
+      tree.x = place + (offset[depth] || 0);
+    }
+    if (nexts[depth] === undefined) {
+      nexts[depth] = 0;
+    }
+    nexts[depth] += 2;
+    tree.mod = offset[depth] || 0;
+  };
+  const addmods = (tree, modsum = 0) => {
+    tree.x = tree.x + modsum;
+    modsum += tree.mod;
+    if (tree.children && tree.children.length > 0) {
+      tree.children.forEach((child) => {
+        addmods(child, modsum);
+      });
+    }
+  };
+  const layout = (tree) => {
+    setup(tree);
+    addmods(tree);
+    return tree;
+  };
+  let tree = new DrawTree(deepCopy(treeData));
+  tree = layout(tree);
+  handleTree(tree)
+  console.log(tree);
   renderTree(tree)
-}
+};
 
 onMounted(() => {
   init();
-  // useAlgorithm1();
+  useAlgorithm1();
   useAlgorithm2()
+  useAlgorithm3();
 });
 </script>
 
